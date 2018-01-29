b8cd5d8773cc0aa7231f1df63c4bd3c2edf1ff9a
vnvkotova <vnv.kotova@gmail.com>
2018-01-29 21:03:51 +0100

    – fixed error with importing in tests

--- gfeat/__init__.py
+++ gfeat/__init__.py
@@ -4,10 +4,10 @@ __author__ = """Ziga Avsec, Jun Cheng, Veronika Kotova"""
 __email__ = 'avsec@in.tum.de, chengju@in.tum.de, kotova@in.tum.de'
 __version__ = '0.1.0'
 
-from . import common_methods
 from . import gene
 from . import genome
 from . import gfeat
 from . import splice_junction
 from . import transcript
 from . import units
+from . import common_methods
