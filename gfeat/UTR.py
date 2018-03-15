22c275f956daa7ebc449cd22c4acff495e539835
vnvkotova <vnv.kotova@gmail.com>
2018-03-15 10:26:08 +0100

    – corrected retrieval of 5'UTR sequences and the corresponding tests – added obtainment of exons of 5'UTRs – corrected all the corresponding tests – added test_FivePrimeUTRSeq_getitem for FivePrimeUTRSeq.__getitem__() magic method

--- gfeat/UTR.py
+++ gfeat/UTR.py
@@ -3,85 +3,119 @@ from gfeat.genome import GFGenome
 
 class FivePrimeUTRSeq:
 
-    # def __init__(self, gtf, fasta = None): #NOTE: added additional parameter - fasta
-    #     """
-    #     constructor
-    #     :param gtf: path to a gtf file
-    #     :return initialises attribute five_prime_utrs – list of strings (5'UTR sequences)
-    #     """
-    #
-    #     gtf_interval = BedTool(gtf)
-    #
-    #     self.five_prime_utrs = []
-    #     if fasta != None:
-    #         for interval in gtf_interval.intervals:
-    #             if interval.fields[2] == "UTR":
-    #                 self.five_prime_utrs.append(interval)
-    #
-    #     return self.five_prime_utrs
-
     def __init__(self, data):
         """
         constructor
-        :param gtf: path to a gtf file # CHANGE IT IF IT WORKS
-        :return initialises attribute five_prime_utrs – list of strings (5'UTR sequences)
+        :param data: GFGenome object
+        :return initialises the following attributes:
+                    seq[] - list of 5' UTR sequences with exons and introns
+                    seq_exons[] - list of 5' UTR sequences with only exons,
+                            NOTE it gives exons corresponding to the 5' UTR, therefore the last corresponding exon gets
+                             cropped at the start_codon_positions[0]
+                    intervals[] - list of 5' UTR intervals
+                    transcripts{} - dictionary: key – transcript id, value – index of the corresponding 5' UTR
+                    exons{} - dictionary: key - 5' UTR index, value - list of tuples (exon sequence, exon interval)
         """
 
-        self.five_prime_utrs = []
+        # NOTE that pyensembles gives another sequence
+        # NOTE 2 5' UTR are considered to be equal only if both their seq and seq_exons are equal
+        self.seq = []
+        self.seq_exons = []
         self.intervals = []
-        self.transcripts = {} # dictionary key – transcript id, value – index of the corresponding 5' UTR
+        self.transcripts = {}
+        self.exons = {}
+
+        count = 0
 
         for contig in data.contigs():
-            for transcript in data.transcripts(contig, "+"): # Todo: add "-" strand
+            for transcript in data.transcripts(contig, '+'):
                 if transcript.contains_start_codon:
-                    if transcript.five_prime_utr_sequence != self.five_prime_utrs:
-                        self.five_prime_utrs.append(transcript.five_prime_utr_sequence)
-                        # BedTool_string = "chr" + contig + " " + transcript.start + " " + \
-                        #     + str(transcript.start + transcript.first_start_codon_spliced_offset)
-                        # self.intrvals.append(BedTool(BedTool_string, from_string=True))
-                        self.intervals.append(Interval(transcript.contig, transcript.start, \
-                                                      int(transcript.start + transcript.first_start_codon_spliced_offset), \
-                                                      "5' UTR", 0))
-                    self.transcripts[transcript.id] = self.five_prime_utrs.index(transcript.five_prime_utr_sequence)
-            for transcript in data.transcripts(contig, "-"): # Todo: add "-" strand
+
+                    temp_seq_exons = ""
+                    temp_exon_list = []
+                    for exon in transcript.exons:
+                        if exon.start <= transcript.start_codon_positions[0]:
+                            if exon.end > transcript.start_codon_positions[0]:
+                                temp_seq_exons = temp_seq_exons + transcript.sequence[exon.start - transcript.start: \
+                                                              transcript.start_codon_positions[0] - transcript.start]
+                                temp_exon_list.append((transcript.sequence[exon.start - transcript.start: \
+                                                         transcript.start_codon_positions[0] - transcript.start], \
+                                                  Interval(transcript.contig, exon.start, transcript.start_codon_positions[0] - 1,\
+                                                           exon.id, 0)))  # Todo dummy score
+                            else:
+                                temp_exon_list.append((transcript.sequence[exon.start - transcript.start: exon.end - transcript.start + 1], \
+                                                  Interval(transcript.contig, exon.start, exon.end, exon.id, 0)))  # Todo dummy score
+                                temp_seq_exons = temp_seq_exons + transcript.sequence[exon.start - transcript.start: \
+                                                                                      exon.end - transcript.start + 1]
+
+                    if transcript.sequence[:transcript.start_codon_positions[0] - transcript.start] not in self.seq \
+                        and temp_seq_exons not in self.seq_exons:
+                        self.seq.append(transcript.sequence[:transcript.start_codon_positions[0] - transcript.start])
+                        self.intervals.append(Interval(transcript.contig, transcript.start, int(transcript.start_codon_positions[0] - 1), "5' UTR", 0)) # Todo dummy score
+
+                        self.seq_exons.append(temp_seq_exons)
+                        self.exons[count] = temp_exon_list
+                        count = count + 1
+
+                    self.transcripts[transcript.id] = self.seq.index(transcript.sequence[:transcript.start_codon_positions[0] - transcript.start])
+
+            for transcript in data.transcripts(contig, '-'):
                 if transcript.contains_start_codon:
-                    if transcript.five_prime_utr_sequence != self.five_prime_utrs:
-                        self.five_prime_utrs.append(transcript.five_prime_utr_sequence)
-                        # BedTool_string = "chr" + contig + " " + transcript.start + " " + \
-                        #     + str(transcript.start + transcript.first_start_codon_spliced_offset)
-                        # self.intrvals.append(BedTool(BedTool_string, from_string=True))
-                        self.intervals.append(interval[transcript.start, transcript.start + \
-                                                  transcript.first_start_codon_spliced_offset])
-                    self.transcripts[transcript.id] = self.five_prime_utrs.index(transcript.five_prime_utr_sequence)
+
+                    temp_seq_exons = ""
+                    temp_exon_list = []
+                    for exon in transcript.exons:
+                        if exon.start <= transcript.start_codon_positions[0]:
+                            if exon.end > transcript.start_codon_positions[0]:
+                                temp_seq_exons = temp_seq_exons + transcript.sequence[exon.start - transcript.start: \
+                                                              transcript.start_codon_positions[0] - transcript.start]
+                                temp_exon_list.append((transcript.sequence[exon.start - transcript.start: \
+                                                         transcript.start_codon_positions[0] - transcript.start], \
+                                                  Interval(transcript.contig, exon.start, transcript.start_codon_positions[0] - 1,\
+                                                           exon.id, 0)))  # Todo dummy score
+                            else:
+                                temp_exon_list.append((transcript.sequence[exon.start - transcript.start: exon.end - transcript.start + 1], \
+                                                  Interval(transcript.contig, exon.start, exon.end, exon.id, 0)))  # Todo dummy score
+                                temp_seq_exons = temp_seq_exons + transcript.sequence[exon.start - transcript.start: \
+                                                                                      exon.end - transcript.start + 1]
+
+                    if transcript.sequence[:transcript.start_codon_positions[0] - transcript.start] not in self.seq \
+                        and temp_seq_exons not in self.seq_exons:
+                        self.seq.append(transcript.sequence[:transcript.start_codon_positions[0] - transcript.start])
+                        self.intervals.append(Interval(transcript.contig, transcript.start, int(transcript.start_codon_positions[0] - 1), "5' UTR", 0)) # Todo dummy score
+
+                        self.seq_exons.append(temp_seq_exons)
+                        self.exons[count] = temp_exon_list
+                        count = count + 1
+
+                    self.transcripts[transcript.id] = self.seq.index(transcript.sequence[:transcript.start_codon_positions[0] - transcript.start])
 
     def __len__(self):
         """
-        :return: the length of the list of 5'UTR sequences – five_prime_utrs
+        :return: the length of the list of 5'UTR sequences – seq
         """
-        return len(self.five_prime_utrs)
+        return len(self.seq)
 
     def __getitem__(self, idx):
         """
         :param idx: integer index of an element of five_prime_utrs
-        :return: dictionary: first entry – utr's sequence,
-        second entry – dictionary: first entry – sequence's interval,
+        :return: dictionary: first entry – 5' UTR's sequence (with both exons and introns),
+        second entry – dictionary: first entry – transcripts dictionary:
+                                        key - "transcripts",
                                     second entry – respective transcript's id
         """
 
         metadata = {}
 
+        transcripts = []
+
         for transcript in self.transcripts:
             if self.transcripts[transcript] == idx:
                 metadata[transcript] = self.intervals[idx]
+                transcripts.append(transcript)
 
         return {
-            "inputs": self.five_prime_utrs[idx],
-            "metadata" : metadata}
-#
-# data = GFGenome(reference_name='hg38_test_FivePrimeUTRSeq',
-#                 annotation_name='hg38_chr22_test_FivePrimeUTRSeq',
-#                 gtf_path_or_url="/Users/veronikakotova/gfeat/tests/data/gencode.v24.annotation_chr22_FivePrimeUTRSeq_testing.gtf",
-#                 transcript_fasta_paths_or_urls="/Users/veronikakotova/gfeat/tests/data/hg38_chr22.fa",
-#                 )
-# FivePrimeUTRSeq_test = FivePrimeUTRSeq(data)
-# print(FivePrimeUTRSeq_test[4])
+            self.seq[idx] : {
+                "transcripts" : transcripts,
+                "exons" : self.exons[idx]
+            }}
